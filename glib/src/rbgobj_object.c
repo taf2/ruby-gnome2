taf2/ruby-gnome2

commit 7d71f21e458327f3ebef38ec805a49844c04b714

* src/rbgobj_object.c: rb_hash_aset()'s key should be VALUE not ID.
git-svn-id: https://ruby-gnome2.svn.sourceforge.net/svnroot/ruby-gnome2/ruby-gnome2/trunk@2740 c539e5d7-1e9e-499f-b274-3850e4969db1

glib/src/rbgobj_object.c
@@ -456,7 +456,7 @@ rbgobj_register_property_setter(gtype, name, func)
     oclass = g_type_class_ref(gtype);
     pspec = g_object_class_find_property(oclass, name);
     
-    rb_hash_aset(table, rb_intern(g_param_spec_get_name(pspec)),
+    rb_hash_aset(table, rb_str_new2(g_param_spec_get_name(pspec)),
                  Data_Wrap_Struct(rb_cData, NULL, NULL, func));
 
     g_type_class_unref(oclass);
@@ -480,7 +480,7 @@ rbgobj_register_property_getter(gtype, name, func)
     oclass = g_type_class_ref(gtype);
     pspec = g_object_class_find_property(oclass, name);
 
-    rb_hash_aset(table, rb_intern(g_param_spec_get_name(pspec)),
+    rb_hash_aset(table, rb_str_new2(g_param_spec_get_name(pspec)),
                  Data_Wrap_Struct(rb_cData, NULL, NULL, func));
 }
 

commit 4206283534ca1f5bb5788c068e4eaf704d7daf51

* src/rbgobj_object.c (gobj_get_property): fix a bug introduced in   r2740. (Gtk::Image#stock returns String not Symbol)
git-svn-id: https://ruby-gnome2.svn.sourceforge.net/svnroot/ruby-gnome2/ruby-gnome2/trunk@3259 c539e5d7-1e9e-499f-b274-3850e4969db1

glib/src/rbgobj_object.c
@@ -558,7 +558,7 @@ gobj_get_property(self, prop_name)
             VALUE table = rb_hash_aref(type_to_prop_getter_table,
                                        INT2FIX(pspec->owner_type));
             if (!NIL_P(table)){
-                VALUE obj = rb_hash_aref(table, rb_intern(g_param_spec_get_name(pspec)));
+                VALUE obj = rb_hash_aref(table, CSTR2RVAL(g_param_spec_get_name(pspec)));
                 if (!NIL_P(obj))
                     Data_Get_Struct(obj, void, getter);
             }